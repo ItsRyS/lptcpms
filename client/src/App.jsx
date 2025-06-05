c7b01d74bca7cd8a434f1edada9f307284c7b486
Patthanan Jaichuai <PatthananLT@gmail.com>
2025-06-06 01:21:10 +0700

    feat: Add react-toastify for notifications and integrate MyToastContainer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"; 
-
+ 
 import AppRoutes from "@/Routes/Routes";
 function App() {
   return (
---
+++ client/src/components/common/MyToastContainer.jsx
@@ -0,0 +1,18 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function MyToastContainer() {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  );
+}
